35927277393d7a735df60007bbc1f5539caddcdd
i-KNPV <knpvinuya@gmail.com>
2024-02-26 01:02:30 +0800

    init: made simple flexbox

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Square from "./components/Square";
+
+// Importing SDGS images
+import square1 from "./assets/SDGS/E-WEB-Goal-1.png";
+import square2 from "./assets/SDGS/E-WEB-Goal-2.png";
+import square3 from "./assets/SDGS/E-WEB-Goal-3.png";
+import square4 from "./assets/SDGS/E-WEB-Goal-4.png";
+import square5 from "./assets/SDGS/E-WEB-Goal-5.png";
+import square6 from "./assets/SDGS/E-WEB-Goal-6.png";
+import square7 from "./assets/SDGS/E-WEB-Goal-7.png";
+import square8 from "./assets/SDGS/E-WEB-Goal-8.png";
+import square9 from "./assets/SDGS/E-WEB-Goal-9.png";
+import square10 from "./assets/SDGS/E-WEB-Goal-10.png";
+import square11 from "./assets/SDGS/E-WEB-Goal-11.png";
+import square12 from "./assets/SDGS/E-WEB-Goal-12.png";
+import square13 from "./assets/SDGS/E-WEB-Goal-13.png";
+import square14 from "./assets/SDGS/E-WEB-Goal-14.png";
+import square15 from "./assets/SDGS/E-WEB-Goal-15.png";
+import square16 from "./assets/SDGS/E-WEB-Goal-16.png";
+import square17 from "./assets/SDGS/E-WEB-Goal-17.png";
 
 function App() {
+  const squarePaths = [
+    square1,
+    square2,
+    square3,
+    square4,
+    square5,
+    square6,
+    square7,
+    square8,
+    square9,
+    square10,
+    square11,
+    square12,
+    square13,
+    square14,
+    square15,
+    square16,
+    square17,
+  ];
+
+  console.log(squarePaths);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="gallery">
+        {squarePaths.map((image, index) => (
+          <Square key={index} image={image} />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/components/Square.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Square = ({ image }) => {
+    return(
+        <div className="square">
+            <img src={image} alt="" />
+        </div>
+    )
+}
+
+export default Square;
