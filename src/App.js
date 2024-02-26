29e6ef319478538d7171077bdb544fdbc872161c
i-KNPV <knpvinuya@gmail.com>
2024-02-26 11:20:36 +0800

    feat: added hover effects and cleaned up image import statements

--- src/App.js
+++ src/App.js
@@ -2,46 +2,21 @@ import { React } from "react";
 import Square from "./components/Square";
 import { motion } from "framer-motion";
 
-// Importing SDGS images
-import square1 from "./assets/SDGS/E-WEB-Goal-1.png";
-import square2 from "./assets/SDGS/E-WEB-Goal-2.png";
-import square3 from "./assets/SDGS/E-WEB-Goal-3.png";
-import square4 from "./assets/SDGS/E-WEB-Goal-4.png";
-import square5 from "./assets/SDGS/E-WEB-Goal-5.png";
-import square6 from "./assets/SDGS/E-WEB-Goal-6.png";
-import square7 from "./assets/SDGS/E-WEB-Goal-7.png";
-import square8 from "./assets/SDGS/E-WEB-Goal-8.png";
-import square9 from "./assets/SDGS/E-WEB-Goal-9.png";
-import square10 from "./assets/SDGS/E-WEB-Goal-10.png";
-import square11 from "./assets/SDGS/E-WEB-Goal-11.png";
-import square12 from "./assets/SDGS/E-WEB-Goal-12.png";
-import square13 from "./assets/SDGS/E-WEB-Goal-13.png";
-import square14 from "./assets/SDGS/E-WEB-Goal-14.png";
-import square15 from "./assets/SDGS/E-WEB-Goal-15.png";
-import square16 from "./assets/SDGS/E-WEB-Goal-16.png";
-import square17 from "./assets/SDGS/E-WEB-Goal-17.png";
+function importAll(r) {
+  let images = {};
+  r.keys().forEach((key, index) => {
+    images[index + 1] = r(key);
+  });
+  return images;
+}
+
+const images = importAll(
+  require.context("./assets/SDGS/", false, /\.(png|jpe?g|svg)$/)
+);
+
+const squarePaths = Object.values(images);
 
 function App() {
-  const squarePaths = [
-    square1,
-    square2,
-    square3,
-    square4,
-    square5,
-    square6,
-    square7,
-    square8,
-    square9,
-    square10,
-    square11,
-    square12,
-    square13,
-    square14,
-    square15,
-    square16,
-    square17,
-  ];
-
   console.log(squarePaths);
 
   return (
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,10 +1,56 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { motion } from "framer-motion";
+
+const ROTATION_RANGE = 32.5;
+const HALF_ROTATION_RANGE = 32.5 / 2;
 
 const Square = ({ image }) => {
+  const ref = useRef(null);
+
+  const [rotateX, setRotateX] = useState(0);
+  const [rotateY, setRotateY] = useState(0);
+
+  const handleMouseMove = (e) => {
+    if (!ref.current) return;
+
+    const rect = ref.current.getBoundingClientRect();
+
+    const width = rect.width;
+    const height = rect.height;
+
+    const mouseX = (e.clientX - rect.left) * ROTATION_RANGE;
+    const mouseY = (e.clientY - rect.top) * ROTATION_RANGE;
+
+    const rY = mouseX / width - HALF_ROTATION_RANGE;
+    const rX = (mouseY / height - HALF_ROTATION_RANGE) * -1;
+
+    setRotateX(rX);
+    setRotateY(rY);
+
+    console.log(rX, rY);
+  };
+
+  const handleMouseLeave = () => {
+    if (!ref.current) return;
+    setRotateX(0);
+    setRotateY(0);
+  };
+
   return (
-    <div style={{ width: "200px", height: "200px" }}>
+    <motion.div
+      ref={ref}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+      style={{
+        transformStyle: "preserve-3d",
+      }}
+      animate={{
+        rotateX,
+        rotateY,
+      }}
+    >
       <img src={image} alt="Square" style={{ width: "100%", height: "100%" }} />
-    </div>
+    </motion.div>
   );
 };
 
