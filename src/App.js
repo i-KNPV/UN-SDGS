1674e8ada4da70d615d692a101a96615f98dcb84
beccajoyce <rrtorralba@up.edu.ph>
2024-02-26 21:50:04 +0800

    added timeline for progress report

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { React } from "react";
 import Square from "./components/Square";
 import { motion } from "framer-motion";
+import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
+import 'react-vertical-timeline-component/style.min.css'
+import './App.css';
 
 function importAll(r) {
   let images = {};
@@ -16,10 +19,12 @@ const images = importAll(
 
 const squarePaths = Object.values(images);
 
+
 function App() {
   console.log(squarePaths);
 
   return (
+    <>
     <div className="container">
       <div className="gallery">
         {squarePaths.map((image, index) => (
@@ -34,7 +39,84 @@ function App() {
           </motion.div>
         ))}
       </div>
-    </div>
+      </div>
+
+
+      <h1 className='title'>
+          Progress Report
+      </h1>
+       <div className="progress">
+        <VerticalTimeline lineColor = '#000'>
+          <VerticalTimelineElement className="tracker" iconStyle = {{background: '#808080', color:'#fff' }} date = '2016' dateClassName="date">
+              <h3 className="report-title">SDG Progress Report 2016</h3>
+              <p id = "content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id velit ex. Etiam in pellentesque eros. Proin blandit faucibus dolor tristique laoreet. 
+              <br/>
+              Proin tincidunt ligula at lacinia malesuada. Mauris vehicula nibh non efficitur dignissim. h</p>
+              <button className="button">Read more</button>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement className="tracker" iconStyle = {{background: '#808080', color:'#fff'}} date = '2017' dateClassName="date">
+              <h3 className="report-title">SDG Progress Report 2017</h3>
+              <p id = "content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id velit ex. Etiam in pellentesque eros. Proin blandit faucibus dolor tristique laoreet. 
+              <br/>
+              Proin tincidunt ligula at lacinia malesuada. Mauris vehicula nibh non efficitur dignissim. h</p>
+              <button className="button">Read more</button>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement className="tracker" iconStyle = {{background: '#808080', color:'#fff'}} date = '2018' dateClassName="date">
+              <h3 className="report-title">SDG Progress Report 2018</h3>
+              <p id = "content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id velit ex. Etiam in pellentesque eros. Proin blandit faucibus dolor tristique laoreet. 
+              <br/>
+              Proin tincidunt ligula at lacinia malesuada. Mauris vehicula nibh non efficitur dignissim. h</p>
+              <button className="button">Read more</button>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement className="tracker" iconStyle = {{background: '#808080', color:'#fff'}} date = '2019' dateClassName="date">
+              <h3 className="report-title">SDG Progress Report 2019</h3>
+              <p id = "content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id velit ex. Etiam in pellentesque eros. Proin blandit faucibus dolor tristique laoreet. 
+              <br/>
+              Proin tincidunt ligula at lacinia malesuada. Mauris vehicula nibh non efficitur dignissim. h</p>
+              <button className="button">Read more</button>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement className="tracker" iconStyle = {{background: '#808080', color:'#fff'}} date = '2020' dateClassName="date">
+              <h3 className="report-title">SDG Progress Report 2020</h3>
+              <p id = "content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id velit ex. Etiam in pellentesque eros. Proin blandit faucibus dolor tristique laoreet. 
+              <br/>
+              Proin tincidunt ligula at lacinia malesuada. Mauris vehicula nibh non efficitur dignissim. h</p>
+              <button className="button">Read more</button>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement className="tracker" iconStyle = {{background: '#808080', color:'#fff'}} date = '2021' dateClassName="date">
+              <h3 className="report-title">SDG Progress Report 2021</h3>
+              <p id = "content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id velit ex. Etiam in pellentesque eros. Proin blandit faucibus dolor tristique laoreet. 
+              <br/>
+              Proin tincidunt ligula at lacinia malesuada. Mauris vehicula nibh non efficitur dignissim. h</p>
+              <button className="button">Read more</button>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement className="tracker" iconStyle = {{background: '#808080', color:'#fff'}} date = '2022' dateClassName="date">
+              <h3 className="report-title">SDG Progress Report 2022</h3>
+              <p id = "content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id velit ex. Etiam in pellentesque eros. Proin blandit faucibus dolor tristique laoreet. 
+              <br/>
+              Proin tincidunt ligula at lacinia malesuada. Mauris vehicula nibh non efficitur dignissim. h</p>
+              <button className="button">Read more</button>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement className="tracker" iconStyle = {{background: '#808080', color:'#fff'}} date = '2023' dateClassName="date">
+              <h3 className="report-title">SDG Progress Report 2023</h3>
+              <p id = "content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id velit ex. Etiam in pellentesque eros. Proin blandit faucibus dolor tristique laoreet. 
+              <br/>
+              Proin tincidunt ligula at lacinia malesuada. Mauris vehicula nibh non efficitur dignissim. h</p>
+              <button className="button">Read more</button>
+          </VerticalTimelineElement>
+        </VerticalTimeline>
+      </div>
+  
+    </>
+    
+      
+  
+
+      
+ 
+
+   
+
+
   );
 }
 
